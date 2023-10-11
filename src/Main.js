3fa23c4048a7729e37681267e6d54dc7f3218661
LeeYoungKyung <lyk1040@gmail.com>
2023-10-11 23:12:53 +0900

    project main

---
+++ src/Main.js
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components";
+
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  padding: 20px;
+`;
+
+const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+`;
+
+const Main1 = styled.div`
+  background-color: #80b3ff;
+  padding: 10px;
+  flex: 1;
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Main2 = styled.div`
+  background-color: #5c748e;
+  padding: 10px;
+  flex: 1;
+  min-height: 100vh;
+  width: 100%; /* 가로 폭을 100%로 설정 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Main3 = styled.div`
+  background-color: #263340;
+  padding: 10px;
+  flex: 1;
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function Main() {
+  return (
+    <MainWrapper>
+      <MainContent>
+        <Main1>Main 1</Main1>
+        <Main2>Main 2</Main2>
+        <Main3>Main 3</Main3>
+      </MainContent>
+    </MainWrapper>
+  );
+}
+
+export default Main;
